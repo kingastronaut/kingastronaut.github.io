201f6077dd0b6e414f6761bade44fc9618ecc43b
kingastronaut <59347076+kingastronaut@users.noreply.github.com>
2024-07-29 14:28:17 +0300

    Update JScript.js

--- JScript.js
+++ JScript.js
@@ -200,8 +200,41 @@
 			document.getElementById("reset-button").hidden = true;
 			document.getElementById("range-info").hidden = true;
 		}
-		
-			
+
+		// mic
+/*
+navigator.mediaDevices.getUserMedia({audio:true})
+    .then(stream => {handlerFunction(stream)})
+
+    function handlerFunction(stream) {
+      rec = new MediaRecorder(stream);
+      rec.ondataavailable = e => {
+        audioChunks.push(e.data);
+        if (rec.state == "inactive"){
+          let blob = new Blob(audioChunks,{type:'audio/mp3'});
+          recordedAudio.src = URL.createObjectURL(blob);
+          recordedAudio.controls=true;
+          recordedAudio.autoplay=true;
+          sendData(blob)
+          }
+        }
+      }
+    
+    function sendData(data) {}
+      record.onclick = e => {
+        record.disabled = true;
+        record.style.backgroundColor = "blue"
+        stopRecord.disabled=false;
+        audioChunks = [];
+        rec.start();
+        }
+      stopRecord.onclick = e => {
+        record.disabled = false;
+        stop.disabled=true;
+        record.style.backgroundColor = "red"
+        rec.stop();
+        }
+*/
 		
 
 		
